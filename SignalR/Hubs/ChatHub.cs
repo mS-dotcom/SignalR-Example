0f4f1b3c5eb7ed0e4afd52b04d57c79d6d34769e
mS-dotcom <56765683+mS-dotcom@users.noreply.github.com>
2022-08-25 16:35:31 +0300

    Add project files.

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Client("dsf").Se
+        }
+    }
+}
